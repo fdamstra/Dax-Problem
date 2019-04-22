84086a9e7ea26834fb6f04eb5a11170b0044e58d
Fred Damstra <fred.damstra@gmail.com>
2019-04-22 14:35:51 -0400

    Removed some unnecessary iter removers.

--- dax_problem_iterative.py
+++ dax_problem_iterative.py
@@ -8,18 +8,20 @@ MAX = 100
 
 def dax_problem(num_people):
     # Ranks is the possible ranks, which is 1 through num_people
-    ranks = list(range(1, num_people+1))
-    rankings = list(permutations(ranks, num_people))
+    ranks = range(1, num_people+1)
+    rankings = permutations(ranks, num_people)
     #print(f'Possible rankings: {rankings}')
-    paired_rankings = list(product(rankings, repeat=2))
+    paired_rankings = product(rankings, repeat=2)
     #print(f'Possible pairs of rankings: {paired_rankings}')
 
     same_averages = 0
+    num_permutations = 0
     for pair in paired_rankings:
         # for each pair of rankings, generate averages for each person
         averages = []
         r1 = pair[0]
         r2 = pair[1]
+        num_permutations += 1
         for i in range(0, num_people):
             averages.append((r1[i]+r2[i])/2)
         # determine if there's any match within the averages by comparing
@@ -33,8 +35,8 @@ def dax_problem(num_people):
         #print(f'Pair {pair[0]}, {pair[1]} [{contains_matches}]: {averages}')
     #print('')
     #print(f'Total: {same_averages} matched out of {len(paired_rankings)} possibilities.')
-    print(f'n = {n}, P = {same_averages}/{len(paired_rankings)} = {same_averages/len(paired_rankings)}')
-    return same_averages/len(paired_rankings)
+    print(f'n = {n}, P = {same_averages}/{num_permutations} = {same_averages/num_permutations}')
+    return same_averages/num_permutations
         
 
 if __name__ == "__main__":
